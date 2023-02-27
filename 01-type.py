515f6773f27fd248254b033da656365110042b4b
IngRodro <rr0213032019@unab.edu.sv>
2023-02-27 10:23:56 -0600

    Function type

---
+++ 01-type.py
@@ -0,0 +1,6 @@
+# Consultar el tipo de dato
+print(type("Hola Python")) # Tipo "str"
+print(type(10)) #Tipo "int"
+print(type(3.14)) #Tipo "float"
+print(type(True)) #Tipo "bool"
+print(type(1 + 3j)) # Tipo "complex"
