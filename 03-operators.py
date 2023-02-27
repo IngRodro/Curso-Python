89efa15ff93f2d9b439f1c7e1ad51f8eb7c26731
IngRodro <rr0213032019@unab.edu.sv>
2023-02-27 13:15:35 -0600

    Operadores en Python

---
+++ 03-operators.py
@@ -0,0 +1,50 @@
+###  Operadores Aritméticos ###
+
+# # Operaciones con números enteros
+
+# print(2 + 2) # Suma
+# print(2 - 2) # Resta
+# print(2 * 2) # Multiplicación
+# print(2 / 2) # División
+# print(2 ** 2) # Potencia
+# print(2 % 2) # Módulo
+# print(2 // 2) # División entera
+# print(2 ** 3 + 3 - 7 / 1 // 2 * 2) # 7.0
+
+# # Operaciones con cadenas de texto
+# print("Hola " + "Mundo") # Concatenación
+# print("Hola " + str(3))
+# print("Hola " * 3) # Repetición
+
+# ###  Operadores de Comparación ###
+
+
+# print(2 == 2) # Igualdad # True
+# print(2 != 2) # Desigualdad # False
+# print(2 > 2) # Mayor que # False
+# print(2 < 2) # Menor que # False
+# print(2 >= 2) # Mayor o igual que # True
+# print(2 <= 2) # Menor o igual que # True
+
+# print("Hola" == "Hola") # True
+# print("Hola" == "hola") # False
+# print("Hola" != "hola") # True
+# print("Hola" > "Mundo") # Orden alfabético Ascii# True
+# print("Hola" < "Mundo") # False
+# print("Hola" >= "Mundo") # False
+# print("Hola" <= "Mundo") # True
+# print("aaaa" > "aaa") # True
+# print("aaaa" < "aaa") # False
+
+###  Operadores Lógicos ###
+
+print(3 > 2 and 2 > 1) # True
+print(3 > 2 and 2 > 4) # False
+print(3 > 2 or 2 > 4) # True
+print(3 < 2 or 2 > 4) # False
+print(not 3 > 2) # False
+print(not 3 < 2) # True
+
+
+
+
