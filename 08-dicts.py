c631f9a786d6784d0fdd349c945472a1c7342026
Rodrigo Ramirez <rr0213032019@unab.edu.sv>
2023-02-27 17:24:29 -0600

    Diccionarios en Python

--- 08-dicts.py
+++ 08-dicts.py
@@ -18,31 +18,56 @@ my_dict = {
     1: 1.74
 }
 
-print(my_other_dict)
-print(my_dict)
+# print(my_other_dict)
+# print(my_dict)
 
-print(len(my_other_dict))
-print(len(my_dict))
+# print(len(my_other_dict))
+# print(len(my_dict))
 
-# Búsqueda
+# # Búsqueda
 
-print(my_dict[1])
-print(my_dict["Nombre"])
+# print(my_dict[1])
+# print(my_dict["Nombre"])
 
-print("Rodrigo" in my_dict)
-print("Apellido" in my_dict)
+# print("Rodrigo" in my_dict)
+# print("Apellido" in my_dict)
 
-# Inserción
+# # Inserción
 
-my_dict["Calle"] = "Calle Principal"
-print(my_dict)
+# my_dict["Calle"] = "Calle Principal"
+# print(my_dict)
 
-# Actualización
+# # Actualización
 
-my_dict["Nombre"] = "Salvador"
-print(my_dict["Nombre"])
+# my_dict["Nombre"] = "Salvador"
+# print(my_dict["Nombre"])
 
-# Eliminación
+# # Eliminación
 
-del my_dict["Calle"]
-print(my_dict)
+# del my_dict["Calle"]
+# print(my_dict)
+
+# Otras operaciones
+
+# print(my_dict.items())
+# print(my_dict.keys())
+# print(my_dict.values())
+
+my_list = ["Nombre", 1, "Piso"]
+
+my_new_dict = dict.fromkeys(my_list)
+print(my_new_dict)
+my_new_dict = dict.fromkeys(("Nombre", 1, "Piso"))
+print((my_new_dict))
+my_new_dict = dict.fromkeys(my_dict)
+print((my_new_dict))
+my_new_dict = dict.fromkeys(my_dict, "Rodrigo")
+print((my_new_dict))
+
+my_values = my_new_dict.values()
+print(type(my_values))
+print(my_values)
+
+print(list(dict.fromkeys(list(my_new_dict.values())).keys()))
+print(tuple(my_new_dict))
+print(set(my_new_dict))
