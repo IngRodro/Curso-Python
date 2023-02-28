77357214eba83866446604690d43a83030445dfc
Rodrigo Ramirez <rr0213032019@unab.edu.sv>
2023-02-28 09:43:53 -0600

    Modulos en Python

---
+++ my_module.py
@@ -0,0 +1,11 @@
+### Módulo para pruebas ###
+
+def sumValue(*numbers):
+    result = 0
+    for number in numbers:
+        result += number
+    print(result)
+
+
+def printValue(value):
+    print(value)
